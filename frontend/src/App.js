27cd87fbb7bedde50000d5c672091b430776aef5
yash gupta <yash3144@gmail.com>
2022-01-06 20:47:48 +0530

    Add opensea and rariable button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./styles/app.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 import nftGif from "./assets/circle.gif";
+import Loading from "./assets/loading.gif";
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
 import myEpicNft from "./artifacts/myEpicNft.json";
@@ -13,12 +14,17 @@ const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [nftAddress, setNftAddress] = useState({
+    openSeaAddress: "",
+    rariableAddress: "",
+    setNft: true,
+  });
   const CONTRACT_ADDRESS = "0xE78C1137b023eaE91830F519E2ab0042c685d6C0";
 
   const askContractToMintNft = async () => {
     try {
       const { ethereum } = window;
-
+      setNftAddress({ ...nftAddress, setNft: false });
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
@@ -113,6 +119,11 @@ const App = () => {
         // If you're familiar with webhooks, it's very similar to that!
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber());
+          setNftAddress({
+            openSeaAddress: `https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`,
+            rariableAddress: `https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`,
+            setNft: true,
+          });
           alert(
             `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
           );
@@ -164,14 +175,33 @@ const App = () => {
             : renderMintNftButton()}
         </div>
         <img className="nft" src={nftGif} alt="nft image" />
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built by @${TWITTER_HANDLE}`}</a>
+        <div>
+          {nftAddress.openSeaAddress && nftAddress.rariableAddress && (
+            <div className="footer-nft-button">
+              <button className="cta-button opensea-button">
+                <a href={nftAddress.openSeaAddress} target="_blank">Click on OpenSea</a>
+              </button>
+              <button className="cta-button rariable-button">
+                <a href={nftAddress.rariableAddress} target="_blank">Click on Rarible</a>
+              </button>
+            </div>
+          )}
+          {nftAddress.setNft === false && (
+            <p className="sub-text">Loading...</p>
+          )}
+          <div className="footer-container">
+            <img
+              alt="Twitter Logo"
+              className="twitter-logo"
+              src={twitterLogo}
+            />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`built by @${TWITTER_HANDLE}`}</a>
+          </div>
         </div>
       </div>
     </div>
